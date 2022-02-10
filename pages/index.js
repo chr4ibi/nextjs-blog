01b2dd816273433526935255b3dc6ae1fe3364a3
CHR4IBI <chraibialipro@gmail.com>
2022-02-10 16:01:37 +0100

    bug fix

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ const Home = ({ allPostsData }) => {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Ali. I'm a student from morocco who learns to code. Contact me </p>
+        <p>Hello, I&apos;m Ali. I&apos;m a student from morocco who learns to code. Contact me </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you&apos;ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
