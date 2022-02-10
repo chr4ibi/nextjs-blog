f3617781fa25f2ab5e4dd978c5507cda9d794484
CHR4IBI <chraibialipro@gmail.com>
2022-02-10 16:03:22 +0100

    bug fix

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
+import utilStyles from '../styles/Utils.module.css'
 import Link from 'next/link'
 
 const name = 'Ali Chraibi'
